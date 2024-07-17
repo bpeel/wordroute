307c13035679d4c6311841d539031d5915847b3b
Neil Roberts <bpeeluk@yahoo.co.uk>
2024-07-17 14:29:44 +0200

    grid_math: Add a function to convert pixels to a grid coordinate

--- src/grid_math.rs
+++ src/grid_math.rs
@@ -86,6 +86,60 @@ impl Geometry {
             self.top_y + y as f32 * self.step_y,
         )
     }
+
+    // Return the hexagon that covers the given coordinates, if there is one
+    pub fn reverse_coords(&self, x: f32, y: f32) -> (u32, u32) {
+        if x < 0.0 || y < 0.0 {
+            return (u32::MAX, u32::MAX);
+        }
+
+        // Offset the y from the top of the points of the top row
+        let y = y - (self.top_y - self.radius);
+        // Offset the x from the leftmost straight part of the grid
+        let x = x - (self.top_x - self.step_x / 2.0);
+
+        // Half the height of the rectangular part in the middle of the hexagon
+        let half_rect_height = self.radius * 0.5;
+
+        let row_height = self.radius -
+            half_rect_height +
+            half_rect_height * 2.0;
+
+        let mut grid_y = (y / row_height) as u32;
+
+        let triangle_height = self.radius - half_rect_height;
+        let y_in_row = y % row_height;
+
+        // Are we in the bit of the grid with the wavy line?
+        if y_in_row < triangle_height {
+            let x_in_hex = if grid_y & 1 == 0 {
+                x % self.step_x
+            } else {
+                (x + self.step_x / 2.0) % self.step_x
+            };
+
+            let row_start = if x_in_hex < self.step_x / 2.0 {
+                triangle_height -
+                    x_in_hex * triangle_height / (self.step_x / 2.0)
+            } else {
+                (x_in_hex - self.step_x / 2.0) *
+                    triangle_height /
+                    (self.step_x / 2.0)
+            };
+
+            if y_in_row < row_start {
+                grid_y = grid_y.wrapping_add_signed(-1)
+            }
+        }
+
+        let mut grid_x = (x / self.step_x) as u32;
+
+        if grid_y & 1 == 1 && x % self.step_x < self.step_x / 2.0 {
+            grid_x = grid_x.wrapping_add_signed(-1);
+        };
+
+        (grid_x, grid_y)
+    }
 }
 
 #[cfg(test)]
@@ -150,5 +204,22 @@ mod test {
 
         assert!((center_x - 8.0).abs() < 0.01);
         assert!((geometry.top_y + geometry.step_y - center_y).abs() < 0.01);
+    }
+
+    #[test]
+    fn reverse_coords() {
+        let grid = Grid::new(".𐑱𐑖𐑩\n𐑼𐑦𐑤𐑯\n𐑦𐑑𐑟𐑮𐑴\n𐑙𐑯𐑨𐑑\n.𐑒𐑼𐑟").unwrap();
+        let geometry = Geometry::new(&grid, 100.0);
+
+        // Outside top left of hexagon
+        assert_eq!(geometry.reverse_coords(24.77678, 1.33928), (0, 4294967295));
+        // Outside top right of hexagon
+        assert_eq!(geometry.reverse_coords(45.982143, 18.303572), (2, 0));
+        // Outside bottom left of hexagon
+        assert_eq!(geometry.reverse_coords(43.30357, 54.6875), (1, 3));
+        // Outside bottom right of hexagon
+        assert_eq!(geometry.reverse_coords(75.22321, 55.13393), (3, 3));
+        // Inside middle rectangle of hexagon
+        assert_eq!(geometry.reverse_coords(8.03571, 28.34821), (4294967295, 1));
     }
 }
